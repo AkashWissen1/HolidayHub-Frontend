b8ff407fbe553e6de716d7c2eaea4c52c35ecf60
Giri-Prasath-V <giri.prasath@wissen.com>
2025-05-21 15:23:11 +0530

    Updated Animation for Login

--- HolidayHub-Frontend/src/pages/Login.jsx
+++ HolidayHub-Frontend/src/pages/Login.jsx
@@ -164,11 +164,23 @@ const Login = () => {
 
   return (
     <div className="login-container">
+      <span class="bub a "></span>
+        <span class="bub b "></span>
+        <span class="bub c "></span>
+        <span class="bub d"></span>
+        <span class="bub e"></span>
+        <span class="bub f"></span>
+        <span class="bub g"></span>
+        <span class="bub h"></span>
+        <span class="bub i"></span>
+        <span class="bub j "></span>
+        <span class="bub k"></span>
       <button onClick={handleBack} className="back-btn">
         ← Back to Home
       </button>
       <div className="login-card">
         <h2>Welcome Back</h2>
+        
         <p className="subtitle">Please enter your credentials to login</p>
 
         <form onSubmit={handleSubmit} className="login-form">
